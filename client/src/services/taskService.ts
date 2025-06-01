453c3fc9f0e3e7626cce22ddad66c4b76c747998
Mahfuzur Rahman <mrahmanlinks@gmail.com>
2025-05-31 21:11:45 -0300

    feat: implement TaskManager UI with global and component-specific CSS

---
+++ client/src/services/taskService.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+export interface Task {
+  _id: string;
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+// GET all tasks
+export const getTasks = async (): Promise<Task[]> => {
+  const response = await axios.get("/tasks");
+  return response.data;
+};
+
+// POST a new task
+export const createTask = async (title: string, description: string) => {
+  try {
+    console.log("I am going to create a new task");
+    const response = await axios.post("/tasks", {
+      title,
+      description,
+      completed: false,
+    });
+    console.log("checking the response", response);
+    console.log("checking the response data", response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error creating task:", error);
+  }
+};
